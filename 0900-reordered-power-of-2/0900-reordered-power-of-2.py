7a3e4d2e37a2e1b91293c94cf49100072e01146c
Saketh Varma K <sakethvarmakalidindi2002@gmail.com>
2025-08-10 10:59:45 -0400

    Time: 0 ms (100%), Space: 17.9 MB (17.56%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.py
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -1,18 +1,11 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-   
-       
         sorted_n = sorted(str(n))
-        
-       
         for i in range(30):
             power_of_two = 1 << i  
-            
-           
             sorted_power_of_two = sorted(str(power_of_two))
         
             if sorted_n == sorted_power_of_two:
                 return True
                 
-    
         return False
