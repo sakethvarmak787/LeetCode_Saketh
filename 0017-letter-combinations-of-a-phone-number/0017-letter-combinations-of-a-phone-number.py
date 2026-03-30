8a744d2aa7bdea98ab65b6ee5aa7662432205b7d
Saketh Varma Kalidindi <sakethvarmakalidindi2002@gmail.com>
2026-03-30 15:15:31 -0400

    Time: 0 ms (100%), Space: 19.5 MB (26.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,26 +4,24 @@ class Solution:
             return []
 
         mapping = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
+            "2": "abc", "3": "def", "4": "ghi",
+            "5": "jkl", "6": "mno",
+            "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
 
-        res = [""]   
+        res = []
 
-        for digit in digits:
-            letters = mapping[digit]
-            new_res = []
+        def backtrack(index, path):
+            # base case: full combination formed
+            if index == len(digits):
+                res.append(path)
+                return
 
-            for word in res:
-                for letter in letters:
-                    new_res.append(word + letter)
+            # get letters for current digit
+            letters = mapping[digits[index]]
 
-            res = new_res
+            for letter in letters:
+                backtrack(index + 1, path + letter)
 
+        backtrack(0, "")
         return res
