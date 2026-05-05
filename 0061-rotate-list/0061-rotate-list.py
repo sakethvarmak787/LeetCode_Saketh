0c559195186b0e7a17d372610411d6e7a549c74b
Saketh Varma Kalidindi <sakethvarmakalidindi2002@gmail.com>
2026-05-05 14:25:26 -0400

    Time: 0 ms (100%), Space: 19.3 MB (74.76%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -3,7 +3,6 @@ class Solution:
         if not head or not head.next or k == 0:
             return head
         
-        # Step 1: find length and tail
         length = 1
         tail = head
         
@@ -11,20 +10,17 @@ class Solution:
             tail = tail.next
             length += 1
         
-        # Step 2: make it circular
         tail.next = head
         
-        # Step 3: reduce k
         k = k % length
         
-        # Step 4: find new tail (length - k - 1 steps)
         steps = length - k
         new_tail = head
         
         for _ in range(steps - 1):
             new_tail = new_tail.next
         
-        # Step 5: break the circle
+   
         new_head = new_tail.next
         new_tail.next = None
         
