19edad717d5f768cff887e1f80aeffd0ba4c3dae
Saketh Varma Kalidindi <sakethvarmakalidindi2002@gmail.com>
2026-05-08 18:18:27 -0400

    Time: 11 ms (18.59%), Space: 19.1 MB (99%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,17 +1,29 @@
 class Solution:
-  def countAndSay(self, n: int) -> str:
-    ans = '1'
-
-    for _ in range(n - 1):
-      nxt = ''
-      i = 0
-      while i < len(ans):
-        count = 1
-        while i + 1 < len(ans) and ans[i] == ans[i + 1]:
-          count += 1
-          i += 1
-        nxt += str(count) + ans[i]
-        i += 1
-      ans = nxt
-
-    return ans
+    def countAndSay(self, n: int) -> str:
+        # Base case for n = 1
+        if n == 1:
+            return "1"
+        
+        # Start with the sequence for n = 1
+        current_str = "1"
+        
+        # Generate the sequence up to n
+        for _ in range(n - 1):
+            next_str = ""
+            i = 0
+            
+            # Use Run-Length Encoding (RLE) logic
+            while i < len(current_str):
+                count = 1
+                # Count consecutive identical characters
+                while i + 1 < len(current_str) and current_str[i] == current_str[i+1]:
+                    count += 1
+                    i += 1
+                
+                # Append the "count" followed by the "digit"
+                next_str += str(count) + current_str[i]
+                i += 1
+            
+            current_str = next_str
+            
+        return current_str
