df65f332a36e97e5f8a2fa9849f13a1b41e0c3b8
Saketh Varma Kalidindi <sakethvarmakalidindi2002@gmail.com>
2026-05-20 15:43:57 -0400

    Time: 54 ms (52.7%), Space: 39.5 MB (78.47%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+
+        seen = {0: -1}
+
+        total = 0
+
+        for i in range(len(nums)):
+
+            total += nums[i]
+
+            rem = total % k
+
+            # if same remainder seen before
+            if rem in seen:
+
+                # subarray length should be at least 2
+                if i - seen[rem] >= 2:
+                    return True
+
+            else:
+                seen[rem] = i
+
+        return False
