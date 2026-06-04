3c7cc7c2e35fbdc0367757d2d227a248200ec489
Saketh Varma Kalidindi <sakethvarmakalidindi2002@gmail.com>
2026-06-04 15:28:25 -0400

    Time: 71 ms (74.82%), Space: 55.2 MB (52.76%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+
+        n = len(nums)
+
+        diff = [0] * (n + 1)
+
+        for l, r in queries:
+            diff[l] += 1
+
+            if r + 1 < n:
+                diff[r + 1] -= 1
+
+        coverage = 0
+
+        for i in range(n):
+            coverage += diff[i]
+
+            if nums[i] > coverage:
+                return False
+
+        return True
